bc9aeec4ca928fa5c041d24f70eb41459473d294
John Michael Brown <jmichaelbrown132737@gmail.com>
2018-10-26 08:46:40 -0700

    allow numberConverter function to work on small numbers

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,15 @@
 function numberConverter(num) {
 
   var computerResponse = ["Beep","Boop","I'm sorry, Dave. I'm afraid I can't do that."]
-  
-  if (num%3 === 0) {
+
+  if (num > 0 && num%3 === 0) {
     return computerResponse[2]
   } else if (num === 1) {
     return computerResponse[1]
   } else if (num === 0) {
     return computerResponse[0];
+  } else {
+    return num;
   }
 }
 
@@ -18,7 +20,7 @@ function numberToComputerArray(numberString) {
   var number = parseInt(numberString);
   var results= [];
 
-  for (var i = 0; i < number; i++) {
+  for (var i = 0; i <= number; i++) {
     results.push(numberConverter(i));
 
   }
@@ -35,20 +37,24 @@ $(function() {
 
   $("#formOne").submit(function(event) {
     event.preventDefault();
-    var trojanHorse = $("#romans").val();
 
-    $(".error-message").hide();
-    if (parseInt(trojanHorse) >= 4000) {
-      $("#tooBig").show();
-    } else if (parseInt(trojanHorse) < 0) {
-      $("#tooLittle").show();
-    } else if (!parseInt(trojanHorse)) {
-      $("#justWrong").show();
-    } else {
-      $(".charriot").slideDown(5000);
-      $("#roman-numeral").text(numberToComputerArray(trojanHorse)).show();
+    var array = [];
+    console.log(array = numberToComputerArray(10));
+    // var trojanHorse = $("#romans").val();
+    //
+    // $(".error-message").hide();
+    // if (parseInt(trojanHorse) >= 4000) {
+    //   $("#tooBig").show();
+    // } else if (parseInt(trojanHorse) < 0) {
+    //   $("#tooLittle").show();
+    // } else if (!parseInt(trojanHorse)) {
+    //   $("#justWrong").show();
+    // } else {
+    //   $(".charriot").slideDown(5000);
+    //   $("#roman-numeral").text(numberToComputerArray(trojanHorse)).show();
+    // }
 
-    }
+
   })
 
     // console.log()
